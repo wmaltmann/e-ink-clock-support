a8171f4205adbd40d180718f9ba6aec7aae9b1ac
Mike Altmann <6716819+wmaltmann@users.noreply.github.com>
2025-06-08 10:41:58 -0500

    updated alarm and wifi icons

--- image-converter/image_converter.py
+++ image-converter/image_converter.py
@@ -62,10 +62,10 @@ def generate_constants(image_paths, output_filename, folder :str, font_const_nam
         f.write("}\n")  # Close dict
 
 # 80px font
-filename = "80/digital_80.py"
-font_const_name = "DIGITAL_80"
-image_files = ["!.jpg","0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "space.jpg", "colon.jpg"]
-folder = "80"
+# filename = "80/digital_80.py"
+# font_const_name = "DIGITAL_80"
+# image_files = ["!.jpg","0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "space.jpg", "colon.jpg"]
+# folder = "80"
 
 # 16px font
 # filename = "16/sans_16.py"
@@ -73,13 +73,13 @@ folder = "80"
 # image_files = ["A.jpg", "M.jpg", "P.jpg", "space.jpg"]
 # folder = "16"
 
-# 16px icons
-# filename = "icons_24/icons_24.py"
-# font_const_name = "ICONS_24"
-# image_files = ["battery_0.jpg", "battery_100.jpg", "battery_25.jpg", "battery_50.jpg",
-#                 "battery_75.jpg", "wifi_on.jpg", "wifi_off.jpg", "wifi_config.jpg" ,"alarm_on.jpg",
-#                 "alarm_off.jpg", "alarm_snooze.jpg","volume_on.jpg", "volume_off.jpg", "vibrate.jpg"]
-# folder = "icons_24"
+# 24px icons
+filename = "icons_24/icons_24.py"
+font_const_name = "ICONS_24"
+image_files = ["battery_0.jpg", "battery_100.jpg", "battery_25.jpg", "battery_50.jpg",
+                "battery_75.jpg", "wifi_on.jpg", "wifi_off.jpg", "wifi_config.jpg" ,"alarm_on.jpg",
+                "alarm_off.jpg", "alarm_snooze.jpg","volume_on.jpg", "volume_off.jpg", "vibrate.jpg"]
+folder = "icons_24"
 
 # 80px icons
 # filename = "icons_80/icons_80.py"
